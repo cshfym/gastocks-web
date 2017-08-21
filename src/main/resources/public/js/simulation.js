3b6b7f4b8f0fd9670925257b1f70a5d9a4ac6dd7
Chuck <cshfym@gmail.com>
2017-08-21 07:30:50 -0600

    Loading drop-down.

--- src/main/resources/public/js/simulation.js
+++ src/main/resources/public/js/simulation.js
@@ -1,4 +1,24 @@
 
+$(document).ready(function() {
+    loadAvailableSimulationsDropDown();
+});
+
+function loadAvailableSimulationsDropDown() {
+
+    $.ajax({
+        url: "http://localhost:9981/gastocks-server/simulation/402881a55e01e784015e01eec66e0000/summary",
+        cache: false,
+        success: function(data) {
+
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+             alert("Error calling /simulation: " + errorThrown);
+             return [];
+         }
+    });
+
+}
+
 function loadSimulationData() {
 
     $.ajax({
@@ -14,6 +34,7 @@ function loadSimulationData() {
           for(var i = 0; i < simulationSummaries.length; i++) {
             var symbolData = {
                 "symbol": simulationSummaries[i].symbol,
+                "total_investment": simulationSummaries[i].totalInvestment,
                 "net_proceeds": simulationSummaries[i].netProceeds,
                 "net_proceeds_pct": simulationSummaries[i].netProceedsPercentage,
                 "gross_proceeds": simulationSummaries[i].grossProceeds,
@@ -31,9 +52,30 @@ function loadSimulationData() {
 }
 
 function setTableData(data) {
-
     $('#tblSimulation').bootstrapTable({
         data: data
     });
+}
 
+function queryParams() {
+    return {
+        type: 'owner',
+        sort: 'updated',
+        direction: 'desc',
+        per_page: 100,
+        page: 1
+    };
+}
+
+function currencyFormatter(value) {
+
+    if (value.toString().startsWith("-")) {
+        return '<span style="color: red;">-$' + value.toString().replace("-","") + '</span>';
+    }
+
+    return '$' + value;
+}
+
+function percentageFormatter(value) {
+    return value + '%';
 }
