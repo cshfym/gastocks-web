94cae62933252c0a60571aa31a4c7f3c868a4296
Chuck <cshfym@gmail.com>
2017-11-23 11:45:31 -0700

    Wire up sector/industry drop-down and search for symbols/vse.

--- src/main/resources/public/js/application.js
+++ src/main/resources/public/js/application.js
@@ -6,6 +6,8 @@ var SIMULATIONS_PATH = "/simulations";
 var SIMULATION_SUMMARY_PATH = "/summary?compact=true"
 var TRANSACTIONS_PATH = "/transactions";
 var TECHNICAL_QUOTE_PATH = "/technicalquote";
+var SECTORS_PATH = "/sectors";
+var INDUSTRIES_PATH = "/industries";
 
 Number.prototype.format = function(n, x) {
     var re = '(\\d)(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
--- src/main/resources/public/js/symbols.js
+++ src/main/resources/public/js/symbols.js
@@ -1,26 +1,85 @@
 
 var loadedSymbolData = [];
 
+var noSectorSelection = "** Sector (Optional) **";
+var noIndustrySelection = "** Industry (Optional) **";
+
 $(document).ready(function() {
-    loadSymbolDropDown();
+    loadAvailableSectorsDropDown();
+    loadAvailableIndustriesDropDown();
+    //loadSymbolTable();
 });
 
-function loadSymbolDropDown() {
+function loadAvailableSectorsDropDown() {
 
+    $("#ddlSectorList").empty();
+
+    var selectOption = "<option value=default>" + noSectorSelection + "</option>";
+    $(selectOption).appendTo("#ddlSectorList");
+
+    $.ajax({
+        type: "GET",
+        url: SERVER_URL + SECTORS_PATH,
+        dataType: "json",
+        success: function (data) {
+            $.each(data, function(i, object) {
+                var option = "<option value=" + object.id + ">" + object.description + "</option>";
+                $(option).appendTo('#ddlSectorList');
+            });
+        }
+    });
+}
+
+function loadAvailableIndustriesDropDown() {
+
+    $("#ddlIndustriesList").empty();
+
+    var selectOption = "<option value=default>" + noIndustrySelection + "</option>";
+    $(selectOption).appendTo("#ddlIndustriesList");
+
+    $.ajax({
+        type: "GET",
+        url: SERVER_URL + INDUSTRIES_PATH,
+        dataType: "json",
+        success: function (data) {
+            var uniqueIndustryNames = [];
+            $.each(data, function(i, el){
+                if($.inArray(el.category, uniqueIndustryNames) === -1) uniqueIndustryNames.push(el.category);
+            });
+            $.each(uniqueIndustryNames, function(i, object) {
+                var option = "<option value=default>" + object + "</option>";
+                $(option).appendTo('#ddlIndustriesList');
+            });
+        }
+    });
+}
+
+function loadSymbolTable() {
+
+    // Price Range
     var min52Weeks = $("#txtMin52WeekPrice").val();
     var max52Weeks = $("#txtMax52WeekPrice").val();
-
-
     if (isNullOrUndefined(min52Weeks)) { min52Weeks = 0; }
     if (isNullOrUndefined(max52Weeks)) { max52Weeks = 9999999999.99; }
+
+    // Sector
+    var selectedSector = $("#ddlSectorList option:selected").text();
+    if (selectedSector.startsWith(noSectorSelection)) {
+        selectedSector = "";
+    }
+
+    // Industry
+    var selectedIndustry = $("#ddlIndustriesList option:selected").text();
+    if (selectedIndustry.startsWith(noIndustrySelection)) {
+        selectedIndustry = "";
+    }
 
     var postData = {
         maxQuotePrice: max52Weeks,
         minQuotePrice: min52Weeks,
-        sector: "Utilities",
-        industryCategory: "",
+        sector: selectedSector,
+        industryCategory: selectedIndustry,
         industrySubCategory: ""
-
     };
 
     $.ajax({
@@ -31,6 +90,7 @@ function loadSymbolDropDown() {
         dataType: "json",
         data: JSON.stringify(postData),
         success: function(symbolData) {
+            setSymbolTableData([]);
             if (symbolData.length == 0) {
                 alert("No symbols found!");
                 return;
